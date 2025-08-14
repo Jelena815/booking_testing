7074e3219aba1cc85b3fb6fb05eee718665002de
Jelena815 <serjelena9@gmail.com>
2025-08-14 09:41:05 +0200

    Added driver configuration and two classes were created BasePage.java, HomePage.java

---
+++ src/test/java/tests/booking.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class booking {
+    @BeforeMethod
+    public void setup(){}
+
+    @AfterMethod
+    public void tearDown(){}
+}
